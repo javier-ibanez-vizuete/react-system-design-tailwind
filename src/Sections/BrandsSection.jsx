fba0cbdcea2c9deb309fa4a1219e750fa4d87e73
Javier Ibañez Vizuete <javieriibanezvizuete@gmail.com>
2025-09-03 19:32:14 +0200

    feat(HomePage): add FeaturesSection to homePage

--- src/Sections/BrandsSection.jsx
+++ src/Sections/BrandsSection.jsx
@@ -1,16 +1,19 @@
 import { ImageContainer } from "../components/ImageContainer";
 import { SectionContainer } from "../components/SectionContainer";
 
+import { useContext } from "react";
 import logoAmazon from "../assets/pictures/home-page/logo-amazon.png";
 import logoCocacola from "../assets/pictures/home-page/logo-cocacola.png";
 import logoFedex from "../assets/pictures/home-page/logo-fedex.png";
 import logoIBM from "../assets/pictures/home-page/logo-ibm.png";
 import logoWalmart from "../assets/pictures/home-page/logo-walmart.png";
+import { LanguageContext } from "../contexts/LanguageContext";
 
 export const BrandsSection = () => {
+    const { getText } = useContext(LanguageContext);
     return (
         <SectionContainer className="flex flex-col gap-8">
-            <h5 className="text-md text-gray-400">Already loved and Trusted by product team</h5>
+            <h5 className="text-md text-gray-400">{getText("brandsSectionTitle")}</h5>
             <div className="grid grid-cols-3  gap-12 place-self-center md:grid-cols-4 lg:grid-cols-5 md:place-self-stretch md:place-items-center">
                 <ImageContainer className={"w-20"}>
                     <img className="w-full" src={logoIBM} alt="Logo IBM" />
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import { Hero } from "../components/Hero";
 import { LanguageContext } from "../contexts/LanguageContext";
 import { BrandsSection } from "../Sections/BrandsSection";
 import { CTAFirstSection } from "../Sections/CTAFirstSection";
+import { FeaturesSection } from "../Sections/FeaturesSection";
 
 export const HomePage = () => {
     const { getText } = useContext(LanguageContext);
@@ -12,6 +13,7 @@ export const HomePage = () => {
             <CTAFirstSection />
             <Hero />
             <BrandsSection />
+            <FeaturesSection />
         </div>
     );
 };
